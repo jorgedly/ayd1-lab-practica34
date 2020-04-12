459e018554e55af2691d05ee87fe6ac30f040f62
Jorge Lopez <jorgekl.1994@gmail.com>
2020-04-11 20:06:28 -0600

    Agregadas importaciones en material.module.ts

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,12 +3,37 @@ import { NgModule } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
 
 const mods = [
     MatButtonModule,
     MatTabsModule,
     MatGridListModule,
-    MatIconModule
+    MatIconModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatListModule,
+    MatSnackBarModule,
+    MatTooltipModule
 ]
 
 @NgModule({
